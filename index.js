6ba04ea6edd5d9f2687417bc8df326130818e0aa
wangzhangsheng(cary) <wb-wzs728893@alibaba-inc.com>
2023-02-20 21:48:22 +0800

    fix: add git fetch and change main to promise

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
+const { resolve } = require("path");
 
 // 填写存放了所有项目仓库的地方
 const root = "/Users/cary/Desktop/test";
@@ -24,36 +25,44 @@ const executeShellCommands = (cmd, options = {}) => {
   });
 };
 
-const main = async (branch = "master", authors = "") => {
+const getData = async (branch = "master", authors = "") => {
   const arrs = [];
   const files = await fs.promises.readdir(root);
-  for (const file of files) {
-    try {
-      const dirPath = path.join(root, file);
-      const stat = await fs.promises.stat(dirPath);
-      if (!stat.isDirectory()) continue;
-      const gitPath = path.join(dirPath, ".git");
-      const packageInfoPath = path.join(dirPath, "package.json");
-      if (!fs.existsSync(packageInfoPath)) continue;
-      const packageInfo = JSON.parse(fs.readFileSync(packageInfoPath));
-      if (!fs.existsSync(gitPath)) continue;
-      const stat2 = await fs.promises.stat(gitPath);
-      if (!stat2.isDirectory()) continue;
-      const _authors = authors.split("|").join(" ");
-      const output = await executeShellCommands(
-        `"./shell.sh" ${dirPath} ${branch} ${_authors}`
-      );
-      console.log(packageInfo?.name, output);
-      arrs.push(`${packageInfo?.name} ${output}`);
-    } catch (e) {
-      console.log(e);
+  return new Promise(async (resolve) => {
+    for (const file of files) {
+      try {
+        const dirPath = path.join(root, file);
+        const stat = await fs.promises.stat(dirPath);
+        if (!stat.isDirectory()) continue;
+        const gitPath = path.join(dirPath, ".git");
+        const packageInfoPath = path.join(dirPath, "package.json");
+        if (!fs.existsSync(packageInfoPath)) continue;
+        const packageInfo = JSON.parse(fs.readFileSync(packageInfoPath));
+        if (!fs.existsSync(gitPath)) continue;
+        const stat2 = await fs.promises.stat(gitPath);
+        if (!stat2.isDirectory()) continue;
+        const _authors = authors.split("|").join(" ");
+        const output = await executeShellCommands(
+          `"./shell.sh" ${dirPath} ${branch} ${_authors}`
+        );
+        console.log(packageInfo?.name, branch, output);
+        arrs.push(`${packageInfo?.name} ${output}`);
+      } catch (e) {
+        console.log(e);
+      }
     }
-  }
-  fs.writeFileSync("./table.txt", arrs.join(""));
+    resolve(arrs);
+  });
+};
+
+const main = async () => {
+  const master = await getData("origin/master", "cary|wangzhangsheng");
+  // const private = await getData("origin/trunk-private", "cary|wangzhangsheng");
+  fs.writeFileSync("./table.txt", [...master].join(""));
 };
 
 try {
-  main("origin/master", "cary|wangzhangsheng");
+  main();
 } catch (e) {
   console.error("We've thrown! Whoops!", e);
 }
--- shell.sh
+++ shell.sh
@@ -4,6 +4,7 @@ cd $1
 # https=$(git remote get-url origin | sed 's/^git@\(.*\):\/*\(.*\).git/https:\/\/\1\/\2.git/')
 ssh=$(git remote get-url origin | sed 's/^https:\/\/\([^\/]*\)\/\(.*\).git/git@\1\:\2.git/')
 git remote set-url origin $ssh
+git fetch
 
 # count lines
 git log $2 --author="\($3\|$4\)" --pretty=tformat: --numstat --no-merges --since="1 year ago" -- . ':(exclude)*yarn.lock*' -- . ':(exclude)*package-lock*' -- . ':(exclude)*generated*' -- . ':(exclude)*schemas*' | awk "{ add += \$1; subs += \$2; loc += \$1 - \$2 } END { printf \"%s %s %s\n\", add, subs, loc }"
